de8d1cb434baf16cacec474dafaf5d8d1fd6a8a3
Yuanzheng (Lucas) Song <song_yuanzheng@hotmail.com>
2025-09-19 17:47:48 -0600

    Updated with JPA Entities

---
+++ Ecommerce-Angular-Springboot/src/main/java/com/ytechtrade/ecommerceangularspringboot/EcommerceAngularSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.ytechtrade.ecommerceangularspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceAngularSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcommerceAngularSpringbootApplication.class, args);
+    }
+
+}
---
+++ Ecommerce-Angular-Springboot/src/test/java/com/ytechtrade/ecommerceangularspringboot/EcommerceAngularSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ytechtrade.ecommerceangularspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceAngularSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
