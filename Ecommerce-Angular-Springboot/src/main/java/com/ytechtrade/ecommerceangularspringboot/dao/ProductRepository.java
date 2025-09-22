5d5aa71ea17c3619a21153a4c727040694354501
Yuanzheng (Lucas) Song <song_yuanzheng@hotmail.com>
2025-09-22 13:03:18 -0600

    Added JPA repostories for product and database

---
+++ Ecommerce-Angular-Springboot/src/main/java/com/ytechtrade/ecommerceangularspringboot/dao/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.ytechtrade.ecommerceangularspringboot.dao;
+
+import com.ytechtrade.ecommerceangularspringboot.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@CrossOrigin("http://localhost:4200")
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
---
+++ Ecommerce-Angular-Springboot/src/main/resources/sql_scripts/Create-database.sql
@@ -0,0 +1,7 @@
+-- -----------------------------------------------------
+-- Schema angular-springboot-ecommerce
+-- -----------------------------------------------------
+DROP SCHEMA IF EXISTS `angular-springboot-ecommerce`;
+
+CREATE SCHEMA `angular-springboot-ecommerce`;
+USE `angular-springboot-ecommerce` ;
