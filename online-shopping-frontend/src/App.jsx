ded83b6046dd27097a602b278d9892d518b676a2
Yuanzheng (Lucas) Song <song_yuanzheng@hotmail.com>
2026-03-09 16:41:33 -0600

    Create onlineshopping React frontend

---
+++ online-shopping-frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import {FaBeer} from "react-icons/fa";
+
+function App() {
+
+  return (
+      <h1 className="flex items-center justify-center h-screen bg-gray-800 text-white text-2xl font-bold">
+          Hello world! <FaBeer />
+      </h1>
+  )
+}
+
+export default App
