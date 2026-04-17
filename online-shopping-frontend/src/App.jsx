fe3c0f0fb641b8fb92db5a8fc001945adfe8777d
Yuanzheng (Lucas) Song <song_yuanzheng@hotmail.com>
2026-04-16 21:22:58 -0600

    Added a Navbar to the landing page

--- online-shopping-frontend/src/App.jsx
+++ online-shopping-frontend/src/App.jsx
@@ -1,13 +1,15 @@
-import React, { useState } from 'react'
+import React, {useState} from 'react'
 import './App.css'
-import { BrowserRouter as Router, Routes, Route}  from "react-router-dom";
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import Home from "./components/home/Home";
 import Products from "./components/products/Products";
+import Navbar from './components/shared/Navbar'
 
 function App() {
     const [count, setCount] = useState(0);
   return (
       <Router>
+          <Navbar />
           <Routes>
               <Route path='/' element={ <Home />}/>
               <Route path='/products' element={ <Products />}/>
