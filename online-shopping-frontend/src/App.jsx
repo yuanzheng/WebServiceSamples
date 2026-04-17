b6cf98c429463ed449bff84ada2dbc461eb899b3
Yuanzheng (Lucas) Song <song_yuanzheng@hotmail.com>
2026-04-17 10:59:41 -0600

    Built the Contact page

--- online-shopping-frontend/src/App.jsx
+++ online-shopping-frontend/src/App.jsx
@@ -4,7 +4,8 @@ import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import Home from "./components/home/Home";
 import Products from "./components/products/Products";
 import Navbar from './components/shared/Navbar'
-import About from "./components/About.jsx";
+import About from "./components/About";
+import Contact from "./components/Contact";
 
 function App() {
     const [count, setCount] = useState(0);
@@ -15,6 +16,7 @@ function App() {
               <Route path='/' element={ <Home />}/>
               <Route path='/products' element={ <Products />}/>
               <Route path='/about' element={ <About />}/>
+              <Route path='/contact' element={ <Contact />}/>
           </Routes>
       </Router>
   )
---
+++ online-shopping-frontend/src/components/Contact.jsx
@@ -0,0 +1,77 @@
+import {FaEnvelope, FaMapMarkedAlt, FaPhone} from "react-icons/fa";
+
+const Contact = () => {
+    return(
+        <div
+            className="flex flex-col items-center justify-center min-h-screen py-12 bg-cover bg-center"
+            style={{backgroundImage: "url('https://images.pexels.com/photos/26268380/pexels-photo-26268380.jpeg?_gl=1*1ykw09s*_ga*ODg3NjU0MzA1LjE3NzY0NDI1NTI.*_ga_8JE65Q40S6*czE3NzY0NDI1NTEkbzEkZzEkdDE3NzY0NDQ3ODAkajQwJGwwJGgw')"}}>
+
+            <div className="bg-white shadow-xl rounded-lg border p-8 w-full max-w-lg">
+                <h1 className="text-4xl font-bold text-center mb-6">Contact us</h1>
+                <p className="text-gray-600 text-center mb-4">
+                    We would love to hear from you! Please fill out the form below or contact us directly
+                </p>
+
+                <form className="space-y-4">
+                    <div>
+                        <label className="block text-sm font-medium text-gray-700">
+                            Name
+                        </label>
+                        <input
+                            type="text"
+                            required
+                            className="mt-1 block w-full border border-gray-300 rounded-lg p-2 focus:outline-hidden focus:ring-2 focus: ring-blue-500"/>
+                    </div>
+
+
+                    <div>
+                        <label className="block text-sm font-medium text-gray-700">
+                            Email
+                        </label>
+                        <input
+                            type="email"
+                            required
+                            className="mt-1 block w-full border border-gray-300 rounded-lg p-2 focus:outline-hidden focus:ring-2 focus: ring-blue-500"/>
+                    </div>
+
+                    <div>
+                        <label className="block text-sm font-medium text-gray-700">
+                            Message
+                        </label>
+                        <textarea
+                            rows="4"
+                            required
+                            className="mt-1 block w-full border border-gray-300 rounded-lg p-2 focus:outline-hidden focus:ring-2 focus: ring-blue-500"/>
+                    </div>
+
+                    <button className="w-full bg-blue-500 text-white py-2 rounded-lg hover:bg-blue-600 transition duration-300">
+                        Send Message
+                    </button>
+                </form>
+
+                <div className="mt-8 text-center">
+                    <h2 className="text-lg font-semibold">Contact Information</h2>
+                    <div className="flex flex-col items-center space-y-2 mt-4">
+                        <div className="flex items-center">
+                            <FaPhone className="text-blue-500 mr-2"/>
+                            <span className="text-gray-600">+4 8961 944 149</span>
+                        </div>
+
+                        <div className="flex items-center">
+                            <FaEnvelope className="text-blue-500 mr-2"/>
+                            <span className="text-gray-600">embarkxofficial@gmail.com</span>
+                        </div>
+
+                        <div className="flex items-center">
+                            <FaMapMarkedAlt className="text-blue-500 mr-2"/>
+                            <span className="text-gray-600">123 Main, Town, USA</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    );
+}
+
+export default Contact;
