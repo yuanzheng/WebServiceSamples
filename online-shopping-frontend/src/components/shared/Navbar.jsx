9ea0d0045e5e6411b4c79a8ec3e936d5f90585d7
Yuanzheng (Lucas) Song <song_yuanzheng@hotmail.com>
2026-04-25 11:58:28 -0600

    Implemented user menu if authenticated

--- online-shopping-frontend/src/components/shared/Navbar.jsx
+++ online-shopping-frontend/src/components/shared/Navbar.jsx
@@ -5,6 +5,7 @@ import {IoIosMenu} from "react-icons/io";
 import {RxCross2} from "react-icons/rx";
 import {Link, useLocation} from "react-router-dom";
 import {useSelector} from "react-redux";
+import UserMenu from "../UserMenu";
 
 const Navbar = () => {
     const path = useLocation().pathname;
@@ -77,7 +78,7 @@ const Navbar = () => {
                     </li>
                     {(user && user.id) ? (
                         <li className="font-medium transition-all duration-150">
-                            <p>Welcome</p>
+                            <UserMenu />
                         </li>
                     ) : (
                         <li className="font-medium transition-all duration-150">
