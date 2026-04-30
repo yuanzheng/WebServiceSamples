556d7f4c6fe4437b687a71c504b742568a3aa638
Yuanzheng (Lucas) Song <song_yuanzheng@hotmail.com>
2026-04-30 12:57:00 -0600

    Added checkout component and add address form features

--- online-shopping-frontend/src/App.jsx
+++ online-shopping-frontend/src/App.jsx
@@ -11,29 +11,31 @@ import Cart from "./components/cart/Cart";
 import PrivateRoute from './components/PrivateRoute'
 import LogIn from "./components/auth/Login";
 import Register from "./components/auth/Register";
+import Checkout from "./components/checkout/Checkout";
 
 function App() {
     const [count, setCount] = useState(0);
-  return (
-      <React.Fragment>
-          <Toaster position='top-center'/>
-          <Router>
-              <Navbar />
-              <Routes>
-                  <Route path='/' element={ <Home />}/>
-                  <Route path='/products' element={ <Products />}/>
-                  <Route path='/about' element={ <About />}/>
-                  <Route path='/contact' element={ <Contact />}/>
-                  <Route path='/cart' element={ <Cart />}/>
-                  <Route path='/' element={<PrivateRoute publicPage />}>
-                      <Route path='/login' element={ <LogIn />}/>
-                      <Route path='/register' element={ <Register />}/>
-                  </Route>
-              </Routes>
-          </Router>
-          <Toaster position='bottom-center'/>
-      </React.Fragment>
-  )
+    return (
+        <React.Fragment>
+            <Toaster position='top-center'/>
+            <Router>
+                <Navbar/>
+                <Routes>
+                    <Route path='/' element={<Home/>}/>
+                    <Route path='/products' element={<Products/>}/>
+                    <Route path='/about' element={<About/>}/>
+                    <Route path='/contact' element={<Contact/>}/>
+                    <Route path='/cart' element={<Cart/>}/>
+                    <Route path='/' element={<PrivateRoute publicPage/>}>
+                        <Route path='/login' element={<LogIn/>}/>
+                        <Route path='/register' element={<Register/>}/>
+                    </Route>
+                    <Route path='/checkout' element={<Checkout/>}/>
+                </Routes>
+            </Router>
+            <Toaster position='bottom-center'/>
+        </React.Fragment>
+    );
 }
 
 export default App
--- online-shopping-frontend/src/store/actions/index.js
+++ online-shopping-frontend/src/store/actions/index.js
@@ -148,3 +148,32 @@ export const registerNewUser
         setLoader(false);
     }
 };
+
+export const addUpdateUserAddress =
+    (sendData, toast, addressId, setOpenAddressModal) => async (dispatch, getState) => {
+        /*
+        const { user } = getState().auth;
+        await api.post(`/addresses`, sendData, {
+              headers: { Authorization: "Bearer " + user.jwtToken },
+            });
+        */
+        dispatch({type: "BUTTON_LOADER"});
+        try {
+            if (!addressId) {
+                const {data} = await api.post("/addresses", sendData);
+            } else {
+                await api.put(`/addresses/${addressId}`, sendData);
+            }
+            dispatch(getUserAddresses());
+            toast.success("Address saved successfully");
+            dispatch({type: "IS_SUCCESS"});
+        } catch (error) {
+            console.log(error);
+            toast.error(error?.response?.data?.message || "Internal Server Error");
+            dispatch({type: "IS_ERROR", payload: null});
+        } finally {
+            setOpenAddressModal(false);
+        }
+    };
+
+
