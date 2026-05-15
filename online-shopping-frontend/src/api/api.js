45dd301ddd2d4e2aee26fbcf8a41aba6a50d7efc
Yuanzheng (Lucas) Song <song_yuanzheng@hotmail.com>
2026-05-14 22:23:35 -0600

    Setup environment files

--- online-shopping-frontend/src/api/api.js
+++ online-shopping-frontend/src/api/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: `${import.meta.env.VITE_DEV_BACK_END_URL}/api`,
-    withCredentials: true,
+  baseURL: `${import.meta.env.VITE_BACKEND_URL}/api`,
+  withCredentials: true,
 });
 
 export default api;
